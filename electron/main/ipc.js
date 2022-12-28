01cba06c941d8e2999ef9a355ae434e326a20d15
Anthony Beaumont <dev.xan105@outlook.be>
2022-12-28 22:12:38 +0700

    sync

--- electron/main/ipc.js
+++ electron/main/ipc.js
@@ -1,22 +1,38 @@
-import { ipcMain } from "electron";
+import { ipcMain, dialog } from "electron";
 
 import { run } from "./game/run.js";
 
 function listen(window){
+
   ipcMain.handle("menu-action", async (event, name, wait = true) => {  
-      switch(name){
-        case "exit":
+      if(name === "exit"){
           window.close();
-          break;
-        case "settings":
-          console.log("settings");
-          break;
-        default:
+      } else {
+      
+        try {
           await run(name, wait, window.minimize);
-          if(wait)
-            window.restore();
-          else
-            window.close();
+        }catch(err){
+          if (err.code === "ENOENT"){
+            const filePath = err.message.replace("spawn","").replace("ENOENT","").trim();
+            dialog.showMessageBoxSync(window, {
+              type: "error",
+              title: "Error", 
+              message: "Failed to spawn process !",
+              detail: `No such file: "${filePath}"`
+            });
+          } else
+            dialog.showMessageBoxSync(window, {
+              type: "error",
+              title: "Error", 
+              message: err.code,
+              detail: err.message
+          });
+        }
+        if(wait)
+          window.restore();
+        else
+          window.close();
+          
       }
   });
 }
--- electron/renderer/components/menu.js
+++ electron/renderer/components/menu.js
@@ -1,7 +1,15 @@
 import { $select } from "@xan105/vanilla-query";
 
+const style =
+`
+
+`;
+
+
 const html = 
 `
+<style>${style}</style>
+
 <aside id="mainMenuSelection">
   <ul class="menu">
     <li><div class="button launch_ffx" data-name="ffx"></div></li>
@@ -15,40 +23,34 @@ const html =
 </aside>
 `;
 
-export default class Component extends HTMLElement {
+export default class WebComponent extends HTMLElement {
+
+  #menu;
 
   constructor() {
     super();
     this.innerHTML = html;
-    this.menu = $select("#mainMenuSelection .menu", this);
-    
-    this.events = [];
+    this.#menu = $select("#mainMenuSelection .menu", this);
   }
 
   connectedCallback() {
-    this.menu.$selectAll("li").forEach((el) => {
-      
-      this.events[0] = this.onClick.bind(this, el);
-      el.$click(this.events[0]);
-      
-      this.events[1] = this.active.bind(this, el);
-      el.$on("mouseenter", this.events[1]);
-      
-      this.events[2] = this.inactive.bind(this, el);
-      el.$on("mouseleave", events[2]); 
+    this.#menu.$selectAll("li").forEach((el) => {
+      el.$click(this.click.bind(this, el));
+      el.$on("mouseenter", this.active.bind(this, el));
+      el.$on("mouseleave", this.inactive.bind(this, el)); 
     });
   }
   
   disconnectedCallback() {
-    this.menu.$selectAll("li").forEach((el) => {  
-      el.$off("click", this.events[0]);
-      el.$off("mouseenter", this.events[1]);
-      el.$off("mouseleave", this.events[2]); 
+    this.#menu.$selectAll("li").forEach((el) => {  
+      el.$off("click");
+      el.$off("mouseenter");
+      el.$off("mouseleave"); 
     });
   }
   
   active(el){
-    this.menu.$select("li.active")?.$removeClass("active");
+    this.#menu.$select("li.active")?.$removeClass("active");
     el.$addClass("active");
   }
   
@@ -56,30 +58,31 @@ export default class Component extends HTMLElement {
     el.$removeClass("active");
   }
   
-  onClick(el){
+  click(el){
     const name = el.$select("div").$attr("data-name");
-    window.ipcRenderer.menuAction(name)
-    .catch((err)=>{ console.error(err) });
+    
+    if (name === "settings"){} //this.dispatch custom event exit
+    else{
+      window.ipcRenderer.menuAction(name).catch(console.error);
+    }
   }
 
   move(climb = false){
     
-    const current = this.menu.$select("li.active") ??
-                    this.menu.$selectAll("li").at(climb ? 1 : -1); //default pos will result in first el in next
+    const current = this.#menu.$select("li.active") ??
+                    this.#menu.$selectAll("li").at(climb ? 1 : -1); //default pos will result in first el in next
                           
     current.$removeClass("active");
 
-    const next = climb ? current.$prev() ?? 
-                 this.menu.$selectAll("li").at(-1) : //restart from bottom
-                 current.$next() ?? 
-                 this.menu.$selectAll("li").at(0); //restart from top
-
+    const next = climb ? current.$prev() : current.$next();
+    
     next.$toggleClass("active");
   }
 
   enter(){
-    const current = this.menu.$select("li.active"); 
-    current?.$removeClass("active")?.$click(); 
+    this.#menu.$select("li.active")?.
+    $removeClass("active")?.
+    $click(); 
   }
   
   onGamepadInput(input){
