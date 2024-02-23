3522c9a61e709beb817e652adcc704e58cf2e70e
Anthony Beaumont <dev.xan105@outlook.be>
2024-02-23 12:23:37 +0700

    eslint flat config

---
+++ eslint.config.js
@@ -0,0 +1,43 @@
+import eslint from "@eslint/js";
+import globals from "globals";
+
+const rules = {
+  ...eslint.configs.recommended.rules,
+  "no-alert": "warn",
+  "no-eval": "error",
+  "require-await": "warn",
+  "no-useless-escape": "warn"
+}
+
+export default [
+  {
+    files: ["electron/main/**/*.js", "build/**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: { ...globals.nodeBuiltin }
+    },
+    rules
+  },
+  {
+    files: ["electron/renderer/**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: { 
+        ...globals.browser,
+        ipcRenderer: "readonly"
+      }
+    },
+    rules
+  },
+  {
+    files: ["electron/renderer/**/*.cjs"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "commonjs",
+      globals: { ...globals.node }
+    },
+    rules
+  }
+];
